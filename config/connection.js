1f7d83395d15b5afdb50ae4569946190cd8e5dba
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-12-20 07:22:11 -0600

    changed host

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,24 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
-
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 // create connection to our database
 // JAWSDB_URL link to Heroku database for when we deploy our app
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "127.0.0.1",
+    dialect: "mysql",
+
+    port: 3306,
+  }
+);
+// }
 
 module.exports = sequelize;
